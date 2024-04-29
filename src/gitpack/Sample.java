115fa2ff4b65673e04a783840c545fb22e733d68
Rammanobi <134794124+Rammanobi@users.noreply.github.com>
2024-04-29 19:33:51 +0530

    Update Sample.java

--- src/gitpack/Sample.java
+++ src/gitpack/Sample.java
@@ -1,16 +1,16 @@
 package gitpack;
 
 class AdditionExample {
-	public int add(int x, int y) {
-		int sum=x+y;
-		return sum;
+	public int add(int x, int y,int z) {
+		int sub=x-y-z;
+		return sub;
 	}
 }
 
 public class Sample {
 	public static void main(String[] args) {
 		AdditionExample obj = new AdditionExample();
-		int result=obj.add(10, 20);
+		int result=obj.add(10, 20,30);
 		System.out.println("Sum of two numbers:"+result);
 	}
 }
